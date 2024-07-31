f27c1a34511d094ed2f6dc987822f25e65987519
Raniere Lima <raniere.lima@mobiis.com.br>
2024-07-31 18:06:59 -0300

    initialization of models and associations

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,19 @@
-import Sequelize from 'sequelize';
-import dbConfig from '../config/database.js';
+import Sequelize from "sequelize";
+import dbConfig from "../config/database.js";
 
-import Product from '../models/product.js'
+import Product from "../models/product.js";
+import Order from "../models/Order.js";
+import OrderItem from "../models/OrderItem.js";
+import OrderStatus from "../models/OrderStatus.js";
 
 const sequelize = new Sequelize(dbConfig);
 
 Product.init(sequelize);
+Order.init(sequelize);
+OrderItem.init(sequelize);
+OrderStatus.init(sequelize);
+
+Order.associate(sequelize.models);
+OrderItem.associate(sequelize.models);
 
 export default sequelize;
