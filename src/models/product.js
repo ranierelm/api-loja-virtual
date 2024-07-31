371b560e4936a7891beb920c895e7f3314f8da5e
Raniere Lima <raniere.lima@mobiis.com.br>
2024-07-30 23:01:22 -0300

    fix formatting

--- src/models/product.js
+++ src/models/product.js
@@ -1,54 +1,56 @@
-import { Model, DataTypes } from 'sequelize';
+import { Model, DataTypes } from "sequelize";
 
 export default class Product extends Model {
-  static init(sequelize){
-    super.init({
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+  static init(sequelize) {
+    super.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        sku: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        ean: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        brand: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        price: {
+          type: DataTypes.DECIMAL(10, 2),
+          allowNull: true,
+        },
+        image_url: {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        stock: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+          defaultValue: 0,
+        },
+        last_purchase_date: {
+          type: DataTypes.DATE,
+          allowNull: true,
+        },
       },
-      sku: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      ean: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      brand: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: true
-      },
-      image_url: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      stock: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        defaultValue: 0
-      },
-      last_purchase_date: {
-        type: DataTypes.DATE,
-        allowNull: true
-      },
-    }, {
+      {
         sequelize,
-        modelName: 'Product',
-        tableName: 'products',
-        underscored: true
-      });
+        modelName: "Product",
+        tableName: "products",
+        underscored: true,
+      }
+    );
   }
 }
-
